a9d0fd7eaa67f2ad3349c5c6df380de5d82188e3
jansdhillon <imightbejan@gmail.com>
2024-10-26 00:41:22 -0600

    justify items start in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
             <div className="flex flex-col justify-between gap-10  min-h-[100dvh]">
               <Nav headerAuth={<HeaderAuth />} />
 
-              <main className="w-full overflow-x-clip flex flex-1 flex-col justify-center mt-20">
+              <main className="w-full overflow-x-clip flex flex-1 flex-col justify-start mt-20">
                 <Feedback>{children}</Feedback>
               </main>
 
