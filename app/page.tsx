61ac449ef232f5f4cb7b3abb02e07d523b9fe6c8
jansdhillon <imightbejan@gmail.com>
2024-10-28 09:20:33 -0600

    update font color

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function HomePage() {
             Find Your Next
             <br /> Great Read.
           </div>
-          <p className="text-base md:text-xl md:max-w-[50%] font-medium  leading-loose">
+          <p className="text-base md:text-xl md:max-w-[50%] font-medium text-muted-foreground  leading-loose">
             Discover a curated selection of rare books. From classics to
             lifestyle books and modern novels,{" "}
             <span className="font-bold">Kathrin&apos;s Books</span> has
