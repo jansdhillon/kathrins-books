9f058e996a4d513676f0fd8d49b72d58edf49910
jansdhillon <imightbejan@gmail.com>
2024-10-29 18:59:06 -0600

    readd service acct credentials

--- app/actions/get-analytics.ts
+++ app/actions/get-analytics.ts
@@ -3,8 +3,22 @@
 import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
 const getAnalytics = async () => {
+  if (!process.env.GA_CLIENT_EMAIL) {
+    console.error("Missing GA_CLIENT_EMAIL");
+    return;
+  }
+
+  if (!process.env.GA_PRIVATE_KEY) {
+    console.error("Missing GA_PRIVATE_KEY");
+    return;
+  }
+
   const analyticsDataClient = new BetaAnalyticsDataClient({
     projectId: process.env.NEXT_PUBLIC_GA_PROJECT_ID,
+    credentials: {
+      client_email: process.env.GA_CLIENT_EMAIL,
+      private_key: JSON.parse(process.env.GA_PRIVATE_KEY!),
+    },
   });
   const [response] = await analyticsDataClient.runReport({
     property: `properties/${process.env.NEXT_PUBLIC_GA_PROPERTY_ID}`,
