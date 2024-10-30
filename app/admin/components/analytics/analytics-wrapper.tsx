ec5959335a4017312844028c2dbf33c8d709e675
jansdhillon <imightbejan@gmail.com>
2024-10-29 22:43:47 -0600

    revert analytics

--- app/admin/components/analytics/analytics-wrapper.tsx
+++ app/admin/components/analytics/analytics-wrapper.tsx
@@ -1,25 +1,10 @@
 "use client";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
-import { Suspense, useEffect, useState } from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import { useEffect, useState } from "react";
 import moment from "moment";
-import Loading from "@/app/loading";
 
-export function AnalyticsWrapper({
-  getAnalytics,
-}: {
-  getAnalytics: () => Promise<any>;
-}) {
+export function AnalyticsWrapper({ getAnalytics } : { getAnalytics: () => Promise<any> }) {
   const [analyticsData, setAnalyticsData] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchAnalytics() {
@@ -27,16 +12,10 @@ export function AnalyticsWrapper({
       console.log("Data from GA", data);
       setAnalyticsData(
         data.rows?.map((row: any) => ({
-          date:
-            row?.dimensionValues &&
-            row.dimensionValues[0]?.value &&
-            moment(row.dimensionValues[0].value, "YYYYMMDD").format(
-              "DD/MM/YYYY"
-            ),
+          date: row?.dimensionValues && row.dimensionValues[0]?.value && moment(row.dimensionValues[0].value, "YYYYMMDD").format("DD/MM/YYYY"),
           users: row?.metricValues && row.metricValues[0]?.value,
         }))
       );
-      setLoading(false);
       console.log("Analytics data", analyticsData);
     }
     fetchAnalytics();
@@ -44,29 +23,23 @@ export function AnalyticsWrapper({
 
   return (
     <div className="w-full flex justify-center items-center">
-      {loading ? (
-        <Loading />
-      ) : (
-        <ResponsiveContainer width="100%" height={400}>
-          <Suspense>
-            <LineChart data={analyticsData}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="date" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                label="users"
-                name="Users"
-                dataKey="users"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-            </LineChart>
-          </Suspense>
-        </ResponsiveContainer>
-      )}
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={analyticsData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" label="date" name="Date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            label="users"
+            name="Users"
+            dataKey="users"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
