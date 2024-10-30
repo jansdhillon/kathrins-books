446eb8c548b5b1ab02002de607f3f75a0ad7d633
jansdhillon <imightbejan@gmail.com>
2024-10-29 19:16:20 -0600

    try to decode ga pk properly

--- app/actions/get-analytics.ts
+++ app/actions/get-analytics.ts
@@ -3,28 +3,36 @@
 import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
 const getAnalytics = async () => {
-  if (!process.env.GA_CLIENT_EMAIL) {
+  const { GA_CLIENT_EMAIL, GA_PRIVATE_KEY_BASE64 } = process.env;
+
+  if (!GA_CLIENT_EMAIL) {
     console.error("Missing GA_CLIENT_EMAIL");
     return;
   }
 
-  if (!process.env.GA_PRIVATE_KEY) {
+  if (!GA_PRIVATE_KEY_BASE64) {
     console.error("Missing GA_PRIVATE_KEY");
     return;
   }
 
+  const GA_PRIVATE_KEY = Buffer.from(GA_PRIVATE_KEY_BASE64, "base64").toString(
+    "utf-8"
+  );
+
   const analyticsDataClient = new BetaAnalyticsDataClient({
     projectId: process.env.NEXT_PUBLIC_GA_PROJECT_ID,
     credentials: {
-      client_email: process.env.GA_CLIENT_EMAIL,
-      private_key: JSON.parse(process.env.GA_PRIVATE_KEY!),
+      client_email: GA_CLIENT_EMAIL,
+      private_key: GA_PRIVATE_KEY,
     },
   });
+
   const [response] = await analyticsDataClient.runReport({
     property: `properties/${process.env.NEXT_PUBLIC_GA_PROPERTY_ID}`,
     dateRanges: [{ startDate: "30daysAgo", endDate: "today" }],
     metrics: [{ name: "activeUsers" }],
   });
+
   return response;
 };
 
