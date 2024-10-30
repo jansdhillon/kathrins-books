49b42d63c393acc422b315cabda4f27acbab55de
jansdhillon <imightbejan@gmail.com>
2024-10-29 18:20:19 -0600

    add project id to try and get analytics working

--- app/actions/get-analytics.ts
+++ app/actions/get-analytics.ts
@@ -2,9 +2,10 @@
 
 import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
-const analyticsDataClient = new BetaAnalyticsDataClient();
-
 const getAnalytics = async () => {
+  const analyticsDataClient = new BetaAnalyticsDataClient({
+    projectId: process.env.NEXT_PUBLIC_GA_PROJECT_ID,
+  });
   const [response] = await analyticsDataClient.runReport({
     property: `properties/${process.env.NEXT_PUBLIC_GA_PROPERTY_ID}`,
     dateRanges: [{ startDate: "30daysAgo", endDate: "today" }],
