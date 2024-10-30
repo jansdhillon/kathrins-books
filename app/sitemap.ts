56fa814b85bd3631797b445e5e3bfc7d47d41cf4
jansdhillon <imightbejan@gmail.com>
2024-10-29 23:55:09 -0600

    add robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,114 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://kathrinsbooks.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://kathrinsbooks.com/books",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://kathrinsbooks.com/search",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.4,
+    },
+    {
+      url: "https://kathrinsbooks.com/shipping",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/cart",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/success",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+    {
+      url: "https://kathrinsbooks.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://kathrinsbooks.com/orders",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/shipping",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.2,
+    },
+    {
+      url: "https://kathrinsbooks.com/error",
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/reset-password",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/shipping",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.2,
+    },
+    {
+      url: "https://kathrinsbooks.com/privacy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/terms",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/sign-in",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/sign-up",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+    {
+      url: "https://kathrinsbooks.com/forgot-password",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+  ];
+}
